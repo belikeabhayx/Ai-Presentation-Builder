afd04d88182a0b485b77ada60aadfe6b69099997
abhay chauhan <chauhanabhay.chauhan2@gmail.com>
2025-02-18 16:11:31 +0530

    feat: add search bar

--- src/components/global/upper-info-bar/index.tsx
+++ src/components/global/upper-info-bar/index.tsx
@@ -1,19 +1,23 @@
-import { SidebarTrigger } from '@/components/ui/sidebar'
-import { User } from '@prisma/client'
-import React from 'react'
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { User } from "@prisma/client";
+import React from "react";
+import SearchBar from "./upper-info-bar";
 
 type Props = {
-    user: User
-    children: React.ReactNode
-}
-const UpperInfoBar = ({user}:Props) => {
+  user: User;
+  children: React.ReactNode;
+};
+const UpperInfoBar = ({ user }: Props) => {
   return (
-    <header className='sticky top-0 z-[10] flex shrink-0 flex-wrap items-center gap-2 border-b bg-background p-4 justify-between'>
-        <SidebarTrigger className='-ml-1'>
-
-        </SidebarTrigger>
+    <header className="sticky top-0 z-[10] flex shrink-0 flex-wrap items-center gap-2 border-b bg-background p-4 justify-between">
+      <SidebarTrigger className="-ml-1" />
+      <Separator className="mr-2 h-4" orientation="vertical" />
+      <div className="w-full max-w-[95%] flex items-center justify-between gap-4 flex-wrap">
+        <SearchBar />
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default UpperInfoBar
+export default UpperInfoBar;
