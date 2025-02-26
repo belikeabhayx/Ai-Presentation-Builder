0d85d87032a2862946fa86cd9c34b924b7422389
abhay chauhan <chauhanabhay.chauhan2@gmail.com>
2025-02-26 16:11:54 +0530

    feat: updated slideStore

--- src/store/useSlideStore.tsx
+++ src/store/useSlideStore.tsx
@@ -1,19 +1,38 @@
-import { Slide, Theme } from "@/lib/types";
+import { ContentItem, Slide, Theme } from "@/lib/types";
 import { Project } from "@prisma/client";
-import { stat } from "fs";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import { v4 } from "uuid";
 
 interface SlideState {
   slides: Slide[];
   project: Project | null;
-  setProject: (project: Project) => void;
+  setProject: (id: Project) => void;
   setSlides: (slides: Slide[]) => void;
+  currentSlide: number;
   currentTheme: Theme;
+  removeSlide: (id: string) => void;
   setCurrentTheme: (theme: Theme) => void;
   getOrderedSlides: () => Slide[];
   reorderSlides: (fromIndex: number, toIndex: number) => void;
+  addSlideAtIndex: (slide: Slide, index: number) => void;
+  setCurrentSlide: (index: number) => void;
+  updateContentItem: (
+    slideId: string,
+    contentItem: string,
+    newContent: string | string[] | string[][]
+  ) => void;
 }
+
+const defaultTheme: Theme = {
+  name: "Default",
+  fontFamily: "Inter, sans-serif",
+  fontColor: "#333333",
+  backgroundColor: "#f0f0f0",
+  slideBackgroundColor: "#ffffff",
+  accentColor: "#3b82f6",
+  type: "light",
+};
 
 export const useSlideStore = create(
   persist<SlideState>(
@@ -25,9 +44,56 @@ export const useSlideStore = create(
       currentTheme: defaultTheme,
       setCurrentTheme: (theme: Theme) => set({ currentTheme: theme }),
       getOrderedSlides: () => {
-        const state = get();
-        return [...state.slides].sort((a, b) => a.slideOrder - b.slideOrder);
+        const state = get()
+        return [...state.slides].sort((a, b) => a.slideOrder - b.slideOrder)
       },
+      addSlideAtIndex: (slide: Slide, index: number) =>
+        set((state) => {
+          const newSlides = [...state.slides];
+          newSlides.splice(index, 0, { ...slide, id: v4() });
+          newSlides.forEach((s, i) => {
+            s.slideOrder = i;
+          });
+
+          return { slides: newSlides, currentSlide: index };
+        }),
+      currentSlide: 0,
+      removeSlide: (id) => {
+        set((state) => ({
+          slides: state.slides.filter((slide) => slide.id !== id),
+        }));
+      },
+      updateContentItem: (slideId, contentId, newContent) =>
+        set((state) => {
+          const updateContentRecursively = (item: ContentItem): ContentItem => {
+            if (item.id === contentId) {
+              return { ...item, content: newContent };
+            }
+            if (
+              Array.isArray(item.content) &&
+              item.content.every((i) => typeof i !== "string")
+            ) {
+              return {
+                ...item,
+                content: item.content.map((subitem) => {
+                  if (typeof subitem !== "string") {
+                    return updateContentRecursively(subitem as ContentItem);
+                  }
+                  return subitem;
+                }) as ContentItem[],
+              };
+            }
+            return item;
+          };
+          return {
+            slides: state.slides.map((slide) =>
+              slide.id === slideId
+                ? { ...slide, content: updateContentRecursively(slide.content) }
+                : slide
+            ),
+          };
+        }),
+      setCurrentSlide: (index: number) => set({ currentSlide: index }),
       reorderSlides: (fromIndex: number, toIndex: number) => {
         set((state) => {
           const newSlides = [...state.slides];
@@ -47,13 +113,3 @@ export const useSlideStore = create(
     }
   )
 );
-
-const defaultTheme: Theme = {
-  name: "Default",
-  fontFamily: "Inter, sans-serif",
-  fontColor: "#333333",
-  backgroundColor: "#f0f0f0",
-  slideBackgroundColor: "#ffffff",
-  accentColor: "#3b82f6",
-  type: "light",
-};
